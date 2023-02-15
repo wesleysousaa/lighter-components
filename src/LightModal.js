35310144e9fb45c318c975575d677c4f8592da39
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2023-02-15 10:56:46 -0300

    feat: add LightModal

---
+++ src/LightModal.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import styles from './LightModal.module.css'
+
+const LightModal = ({ open, children, onClose }) => {
+
+  function closeWithModalAreaClick(e) {
+    if (e.className.includes('modalArea')) {
+      onClose()
+    }
+  }
+
+  return (
+    <>
+      {open && (
+        <div className={styles.modalArea} value={open} onClick={(e) => closeWithModalAreaClick(e.target)}>
+          <div className={styles.modal}>
+            {children && children}
+            {!children && (
+              <>
+                <p className={styles.aviso}>*Atenção, seu modal está sem corpo</p>
+                <h2>Titulo do Modal</h2>
+                <p>Descrição do modal</p>
+                <h3>Corpo</h3>
+
+              </>
+            )}
+          </div>
+        </div>
+
+      )}
+    </>
+  )
+}
+
+export default LightModal
