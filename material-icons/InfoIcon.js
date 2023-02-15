96a32e925e4083fb442185ba854b9fe5718dbcc5
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2023-02-15 19:54:19 -0300

    chore: build MaterialIcons

---
+++ material-icons/InfoIcon.js
@@ -0,0 +1,28 @@
+var $b7OxR$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$defineInteropFlag(module.exports);
+
+$parcel$export(module.exports, "default", () => $b6148c714b8bd648$export$2e2bcd8739ae039);
+
+
+const $b6148c714b8bd648$var$InfoIcon = ({ color: color  })=>{
+    return /*#__PURE__*/ (0, $b7OxR$reactjsxruntime.jsx)("img", {
+        src: color === "white" ? "https://user-images.githubusercontent.com/47366440/219195597-0e9c27f2-e453-475d-ba32-2244f6609a21.png" : "https://user-images.githubusercontent.com/47366440/219195592-f29b4ce8-327b-45bf-9638-8ba51da48352.png",
+        alt: color ? "InfoIcon" + color : "InfoIconDark",
+        style: {
+            width: "2em"
+        }
+    });
+};
+var $b6148c714b8bd648$export$2e2bcd8739ae039 = $b6148c714b8bd648$var$InfoIcon;
+
+
+//# sourceMappingURL=InfoIcon.js.map
